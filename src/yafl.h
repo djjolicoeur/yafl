c7209fa0b808e58f5e2212329528b48860f6059d
Daniel Jolicoeur <djjolicoeur@gmail.com>
2014-06-15 14:16:16 -0400

    cleanup header file

--- src/yafl.h
+++ src/yafl.h
@@ -172,27 +172,6 @@ mpc_parser_t* Expr;
 mpc_parser_t* Yafl;
 
 
-/*void gen_parsers(mpc_parser_t* Number,
-                 mpc_parser_t* Symbol,
-                 mpc_parser_t* String,
-                 mpc_parser_t* Comment,
-                 mpc_parser_t* Sexpr,
-                 mpc_parser_t* Qexpr,
-                 mpc_parser_t* Expr,
-                 mpc_parser_t* Yafl);
-*/
-
 void gen_parsers();
-
-/*
-void teardown_parsers(mpc_parser_t* Number,
-                      mpc_parser_t* Symbol,
-                      mpc_parser_t* String,
-                      mpc_parser_t* Comment,
-                      mpc_parser_t* Sexpr,
-                      mpc_parser_t* Qexpr,
-                      mpc_parser_t* Expr,
-                      mpc_parser_t* Yafl);
-*/
 
 void teardown_parsers();
